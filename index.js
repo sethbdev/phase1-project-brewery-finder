979e9941685fe6235e63072087393460fb6eb860
Seth <seth.bbrendle@gmail.com>
2022-09-14 10:24:52 -0400

    created function to create slider cards, loop through breweries and append to dom

--- index.js
+++ index.js
@@ -5,30 +5,17 @@
 function fetchBreweries(){
 fetch ("https://api.openbrewerydb.org/breweries")
     .then(r => r.json())
-    .then(data => renderBreweries(data))
-}
-//fetchBreweries();
-
-function renderBreweries(breweries){
-    breweries.map(brewery => {
-        //console.log(brewery.name)
-        const breweryBar = document.querySelector(".brewery_bar")
-        const breweryCard = document.createElement("div")
-        breweryBar.append(breweryCard)
-        const breweryName = document.createElement("h3")
-        const breweryAddress = document.createElement('p')
-        const street = brewery.street
-        breweryName.textContent = brewery.name
-        breweryCard.append(breweryName)
-        breweryAddress.textContent = brewery.street
-        breweryCard.appendChild(breweryAddress)
-
+    .then(data => {
+       // renderBreweries(data)
+        renderCarouselCards(data)
     })
 }
+fetchBreweries();
 
 const swiper = new Swiper(".slide_content", {
     slidesPerView: 3,
     spaceBetween: 25,
+    slidesPerGroup: 3,
     loop: true,
     centerSlide: 'true',
     fade: 'true',
@@ -56,20 +43,66 @@ const swiper = new Swiper(".slide_content", {
         },
     }
   });
+
+  const divInsert = document.querySelector("#divInsert");
+
+  function renderCarouselCards(breweries) {
+      console.log(breweries)
+      breweries.forEach((brewery) => {
+          const cardSwiperSlide = document.createElement("div")
+          cardSwiperSlide.className = "card swiper-slide"
+          const imageContent = document.createElement("div")
+          imageContent.className = "image_content"
+          const overlay = document.createElement("span")
+          overlay.className = "overlay"
+          const cardImage = document.createElement("div")
+          cardImage.className = "card_image"
+          const imageTag = document.createElement("img")
+          imageTag.className = "card_image"
+          imageTag.src = "./pictures/bar 2.jpeg"
+  
+          const cardContent = document.createElement("div")
+          cardContent.className = "card_content"
+  
+          const name = document.createElement("h2")
+          name.className = "name"
+          name.textContent = brewery.name
+          cardContent.append(name)
+          
+          console.log(brewery.name)
+  
+          cardImage.append(imageTag)
+  
+          imageContent.append(overlay, cardImage)
+          cardSwiperSlide.append(imageContent, cardContent)
+          divInsert.append(cardSwiperSlide)
+  
+      })
+  
+  
+  }
+
+/*function renderBreweries(breweries){
+    breweries.map(brewery => {
+        //console.log(brewery.name)
+        const breweryBar = document.querySelector(".brewery_bar")
+        const breweryCard = document.createElement("div")
+        breweryBar.append(breweryCard)
+        const breweryName = document.createElement("h3")
+        const breweryAddress = document.createElement('p')
+        const street = brewery.street
+        breweryName.textContent = brewery.name
+        breweryCard.append(breweryName)
+        breweryAddress.textContent = brewery.street
+        breweryCard.appendChild(breweryAddress)
+
+    })
+}
+*/
+
+
 /*
-                        <div class="card swiper-slide">
-                            <div class="image_content">
-                                <span class="overlay"></span>
-
-                                <div class="card_image">
-                                    <img src="./pictures/bar 1.jpeg" alt="bar" class="card_image">
-                                </div>
-                            </div>
-
-                            <div class="card_content">
-                                <h2 class="name">NAME</h2>
-                            </div>
-                        </div>
+                        
 
       function renderBreweries(breweries){
     breweries.map(brewery => {
@@ -90,7 +123,7 @@ const swiper = new Swiper(".slide_content", {
 }
 */
 
-function renderBreweryNames(breweries){
+/*function renderBreweryNames(breweries){
     breweries.map(brewery => {
         //console.log(brewery.name)
         const breweryBar = document.querySelector(".brewery_bar")
@@ -106,10 +139,28 @@ function renderBreweryNames(breweries){
 
     })
 }
-
+*/
 /* need to build a function that loops through the brewery obj,
         for (let i = 0; i < breweries.length; i += 1)
-    uses const sendNamesToH2Divs = document.getElementById(`${[i]}`);
+    uses const sendNamesToH2Divs = document.getElementById(`${"[i]"}`);
     increments plus 1 for each item in in breweries.map,
     each time is runs through: sendNamesToH2Divs.textContent = brewery.name
             This is to append each name to each card on the slider
+
+*/
+
+                        /*<div class="card swiper-slide">
+                            <div class="image_content">
+                                <span class="overlay"></span>
+
+                                <div class="card_image">
+                                    <img src="./pictures/bar 1.jpeg" alt="bar" class="card_image">
+                                </div>
+                            </div>
+
+                            <div class="card_content">
+                                <h2 class="name" i>NAME</h2>
+                            </div>
+                        </div>
+
+                        */
